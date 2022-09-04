0f1c7ea67faafe6d3f97e579f0166c917d8d2a82
mykola-shorobura-omio <mykola.shorobura_ges@omio.com>
2022-09-04 21:49:41 +0200

    latest changes

--- app.py
+++ app.py
@@ -5,17 +5,25 @@ import plotly.graph_objects as go
 import pandas as pd
 import plotly.express as px
 
+
 df = pd.read_csv('ukr_stats.csv')
 fig = px.bar(df, x="Month", y="Quantity", text_auto="")
 fig.update_layout(legend=go.layout.Legend(x=1, y=1, traceorder='normal', font=dict(family='Verdana', size=22, color='black')))
 fig.update_traces(textfont_size=12, textangle=0, textposition="outside", cliponaxis=False)
 
 
-app = Dash(__name__, external_stylesheets=[dbc.themes.LUX])
+app = Dash(__name__)
 app.title = 'Шо по русні?!'.upper()
 app.layout = html.Div([
   dbc.Row([
-    html.H3("Графік кількісті \"хороших\" рускіх.".upper(),
+    html.H3("Графік КІЛЬКОСТІ \"хороших\" ускіх",
+            style={
+              'text-align': 'center',
+              'marginTop': 15
+            })
+  ]),
+  dbc.Row([
+    html.H3("DEACTIVATED russia enemies BY ARMED FORCES OF UKRAINE",
             style={
               'text-align': 'center',
               'marginTop': 15
@@ -33,5 +41,5 @@ app.layout = html.Div([
 
 
 if __name__ == '__main__':
-  server_port = os.environ.get('PORT', '8080')
-  app.run(debug=False, port=server_port, host='0.0.0.0')
+    server_port = os.environ.get('PORT', '8080')
+    app.run(debug=False, port=server_port, host='0.0.0.0')
